40963534b80bea11d20b2a7da9ae147542528152
Nicolas Barrero <nicofelipe26@hotmail.com>
2023-09-26 15:54:22 -0500

    End of exercise 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,11 +1,8 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = { name: "Fundamentals of React", exercises: 10 };
+  const part2 = { name: "Using props to pass data", exercises: 7 };
+  const part3 = { name: "State of a component", exercises: 14 };
 
   const Header = (props) => <h1>{props.course}</h1>;
   const Part = (props) => (
@@ -16,15 +13,15 @@ const App = () => {
   const Content = (props) => (
     <>
       <Part
-        part={props.argument[0].part}
+        part={props.argument[0].name}
         exercises={props.argument[0].exercises}
       />
       <Part
-        part={props.argument[1].part}
+        part={props.argument[1].name}
         exercises={props.argument[1].exercises}
       />
       <Part
-        part={props.argument[2].part}
+        part={props.argument[2].name}
         exercises={props.argument[2].exercises}
       />
     </>
@@ -38,17 +35,11 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content
-        argument={[
-          { part: part1, exercises: exercises1 },
-          { part: part2, exercises: exercises2 },
-          { part: part3, exercises: exercises3 },
-        ]}
-      />
+      <Content argument={[part1, part2, part3]} />
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
       />
     </>
   );
