e7b9c3606fcb57a468d10a3d79227728a7a2b54a
Nicolas Barrero <nicofelipe26@hotmail.com>
2023-09-30 14:45:51 -0500

    End of the exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,21 +6,42 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [avg, setAvg] = useState(0);
+  const [percent, setPercent] = useState(0);
 
   const handleGood = () => {
     console.log("Click Good");
-    const goodVote = good + 1;
-    setGood(goodVote);
+    const newGood = good + 1;
+    setGood(newGood);
+    const newTotal = bad + neutral + newGood;
+    setTotal(newTotal);
+    const newAvg = (newGood - bad) / newTotal;
+    setAvg(newAvg);
+    const newPercent = (newGood * 100) / newTotal;
+    setPercent(newPercent);
   };
   const handleNeutral = () => {
     console.log("Click neutral");
-    const neutralVote = neutral + 1;
-    setNeutral(neutralVote);
+    const newNeutral = neutral + 1;
+    setNeutral(newNeutral);
+    const newTotal = bad + newNeutral + good;
+    setTotal(newTotal);
+    const newAvg = (good - bad) / newTotal;
+    setAvg(newAvg);
+    const newPercent = (good * 100) / newTotal;
+    setPercent(newPercent);
   };
   const handleBad = () => {
     console.log("Click bad");
-    const badVote = bad + 1;
-    setBad(badVote);
+    const newBad = bad + 1;
+    setBad(newBad);
+    const newTotal = newBad + neutral + good;
+    setTotal(newTotal);
+    const newAvg = (good - newBad) / newTotal;
+    setAvg(newAvg);
+    const newPercent = (good * 100) / newTotal;
+    setPercent(newPercent);
   };
   return (
     <>
@@ -29,9 +50,12 @@ function App() {
       <Button handleClick={handleNeutral} text={"Neutral"} />
       <Button handleClick={handleBad} text={"Bad"} />
       <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {avg}</p>
+      <p>Positive: {percent} %</p>
     </>
   );
 }
