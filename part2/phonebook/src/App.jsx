1de17c45453393183c10cbceb21541f04c3af5bb
Nicolas Barrero <nicofelipe26@hotmail.com>
2024-05-12 20:40:31 -0400

    End of exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", key: 0 }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", key: 0, phone: 1234 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     console.log("Click Submit");
     event.preventDefault();
 
     const arrNames = persons.map((person) => person.name.toLowerCase());
-    console.log(arrNames);
+    const arrNum = persons.map((person) => person.phone);
+    console.log(arrNum);
 
     if (arrNames.includes(newName.toLowerCase())) {
-      alert(`${newName} is already added to the phone book`);
+      alert(`The person ${newName} is already added to the phone book`);
+    } else if (arrNum.includes(newNumber)) {
+      alert(`Phone number ${newNumber} is already added to the phone book`);
     } else {
       const nameObj = {
-        name: newName,
         key: persons.length + 1,
+        name: newName,
+        phone: newNumber,
       };
       setPersons(persons.concat(nameObj));
       setNewName("New?");
@@ -28,6 +35,13 @@ const App = () => {
     console.log("Previous", newName);
     console.log("New", event.target.value);
   };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+    console.log("Previous", newNumber);
+    console.log("New", event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -35,15 +49,24 @@ const App = () => {
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
         </div>
+        <div>
+          Phone: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
         {/* <div>debug: {persons.name}</div> */}
         <div>
-          {newName ? <button type="submit">Add</button> : "Write a name"}
+          {newName && newNumber ? (
+            <button type="submit">Add</button>
+          ) : (
+            "Write a name"
+          )}
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.key}>{person.name}</li>
+          <li key={person.key}>
+            {person.name}:{person.phone}
+          </li>
         ))}
       </ul>
     </div>
